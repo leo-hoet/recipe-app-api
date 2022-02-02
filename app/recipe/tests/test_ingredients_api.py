16d3d4c77c8f0b631c66fab1cbfd8b9616219470
Leo Hoet <lalfonso@optiwe.com>
2022-02-02 11:33:24 -0300

    Added list ingredient

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -65,3 +65,35 @@ class PrivateIngredientApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], ingredient.name)
+
+    def test_create_ingredient_succesful(self):
+        """Test creating a valid ingredient"""
+
+        payload = {
+            'name': 'Cabbage'
+        }
+
+        res = self.client.post(INGREDIENTS_URL, payload)
+        exists = Ingredient.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(exists)
+
+    def test_create_ingredient_invalid(self):
+        """Test creating a valid ingredient"""
+
+        payload = {
+            'name': ''
+        }
+
+        res = self.client.post(INGREDIENTS_URL, payload)
+        exists = Ingredient.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertFalse(exists)
